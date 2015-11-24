122adb1f3ce08ba424294846b5426c63f6e21c64
enriquesleon <enriquesleon>
2015-11-23 23:31:15 -0800

    the way of the future

--- thegits/routes/stormpath/stormpath.js
+++ thegits/routes/stormpath/stormpath.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var stormpath = require('express-stormpath');
 var stormpathController = require('../../controllers/stormpath/stormpath');
+var tempcontroller = require('../../controllers/users/users');
 var array = [];
 
 router.get('/account',stormpathController.getAcountInfo);
-router.post('/create',stormpathController.createAccount);
+router.post('/create',  tempcontroller.createAccount/*stormpathController.createAccount*/);
 module.exports = router;
