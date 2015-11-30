a81e1866b8acc4f660b707de6516f1c72d3f9c76
Genaro Orodio Jr <genaroorodio@aol.com>
2015-11-30 14:50:43 -0800

    made reports into one page with modal

--- thegits/public/app/javascripts/app.module.js
+++ thegits/public/app/javascripts/app.module.js
@@ -53,7 +53,7 @@ app.config([
         })
 
         .state('report', {
-            url: '/report/create',
+            url: '/report',
             views: {
                 'main-view': {
                     templateUrl: '/app/javascripts/templates/report.html',
@@ -65,21 +65,6 @@ app.config([
                 }
             }
         })
-
-    	.state('viewreport', {
-            url: '/report/view',
-            views: {
-                'main-view': {
-                    templateUrl: '/app/javascripts/templates/reportsview.html',
-                    controller: 'ReportingCtrl'
-                },
-                'navigation-bar': {
-                    templateUrl: '/app/javascripts/templates/shared/navigationbar.html',
-                    controller: 'NavigationBarCtrl'
-                }
-            }
-        });
-
 
         $urlRouterProvider.otherwise('home');
     }
