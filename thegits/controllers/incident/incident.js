b198c54afb118c217aeba5754e6db5ec71f4d1fd
enriquesleon <enriquesleon>
2015-12-06 23:49:32 -0800

    added seprate querying for all incidents for city crew

--- thegits/controllers/incident/incident.js
+++ thegits/controllers/incident/incident.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var ObjectId = require('mongoose').Types.ObjectId; 
+var stormpath = require('express-stormpath');
 
 var Incident = require('../../models/incident/incident');
 /*Mongoose actually validates input for the model no need to check for each attribute*/
@@ -27,7 +28,8 @@ module.exports.createIncident = function(req,res){
 };
 
 module.exports.getIncident = function(req,res){
-	Incident.findOne({ '_id': req.params.id }, function(error, incident) {
+	var userType = req.baseUrl.split('/')[2];
+    Incident.findOne({ '_id': req.params.id }, function(error, incident) {
   		if(error){
             res.json(error);
         }
@@ -67,18 +69,41 @@ module.exports.modifyIncident = function(req,res){
     }
 };
 module.exports.getAllIncidents = function(req,res){
-    Incident
-    .find()
-    .populate('suspects')
-    .exec(function(err,incidents){
-        if (err){
-            console.log(err);
-            res.sendStatus(404);
+    
+    var userType = req.baseUrl.split('/')[2];
+    var projection;
+    var cityCrewQuery;
+    console.log(req.user);
+    req.user.getGroups(function(err,groups){
+        if(err){
+            res.sendStatus(400);
         }
-        else{
-            res.send(incidents);
+        else if(groups.items[0]){
+            userGroup =  groups.items[0].name;
+            if (userGroup == "City"){
+                console.log(req.user.customData.employeeNumber);
+                cityCrewQuery = {cityCrewId:req.user.customData.employeeNumber};
+                projection = {
+                    suspects:0,
+                    lawEnforcementEmployeeNumber:0
+                }
+            }
+            Incident
+            .find(cityCrewQuery)
+            .populate('suspects')
+            .select(projection)
+            .exec(function(err,incidents){
+                if (err){
+                    console.log(err);
+                    res.sendStatus(404);
+                }
+                else{
+                    res.send(incidents);
+                }
+            });
         }
     });
+
 };
 
 convertToObjectId = function(models){
